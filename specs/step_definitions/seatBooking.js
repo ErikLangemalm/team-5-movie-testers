bab100c509ecd415e7bc5869b544a44831860c23
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2024-01-29 13:45:51 +0100

    Made steps for user storie nr 5

---
+++ specs/step_definitions/seatBooking.js
@@ -0,0 +1,31 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('that I am on the first page', () => {
+  // TODO: implement step
+  cy.visit('/');
+});
+
+When('I click on a timeslot', () => {
+  // TODO: implement step
+  cy.get()
+});
+
+When('I click on a specifik timeslot', () => {
+  // TODO: implement step
+});
+
+When('I reserve a seat', () => {
+  // TODO: implement step
+});
+
+When('I select {string}', (a) => {
+  // TODO: implement step
+});
+
+When('I select a seat', () => {
+  // TODO: implement step
+});
+
+Then('That seat should be unavailable for other users(repeat and verify previous steps).', () => {
+  // TODO: implement step
+});
