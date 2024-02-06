237b035cb6fb240413313d5fd0a47bacc38f2cbc
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2024-02-06 17:33:13 +0100

    Update ageRestriction.js

--- specs/step_definitions/ageRestriction.js
+++ specs/step_definitions/ageRestriction.js
@@ -8,9 +8,14 @@ Given('that I am on the start page', () => {
 
 Given('I have logged in', () => {
   // TODO: implement step
+  let email = "cifap38802@evvgo.com";
+  let password = "123456";
   cy.get('#basic-nav-dropdown').click({ force: true });
   cy.get('line-object.text-decoration-none.text-primary.dropdown-item').click({ force: true });
-  cy.get('.form-control.with-icon').click({ force: true })
+  cy.get('.form-control.with-icon').eq(0).click({ force: true });
+  cy.get('.form-control.with-icon').eq(0).type(email);
+  cy.get('.form-control.with-icon').eq(1).click({ force: true });
+  cy.get('.form-control.with-icon').eq(1).type(password);
 });
 
 When('I click on a movie that has an age restriction of 15 years old', () => {
