97e4681242e470e1039a96d572edc7e70c061f05
DevAnamTales <Anam-rehman@live.com>
2024-02-07 12:59:34 +0100

    Create movie_detail.js
    Write step definitions for movie_detail.feature file

---
+++ specs/step_definitions/movie_detail.js
@@ -0,0 +1,35 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import baseUrl from "../../baseUrl";
+
+Given('I am on the home page', () => {
+  cy.visit('/');
+});
+
+When('I click on the movie titled {string}', (movieName) => {
+  cy.get('.mb-4.container>div>div')
+    .should("be.visible")
+    .contains(movieName)
+    .click({ force: true });
+});
+
+Then('I should be directed to the movie details page', () => {
+  cy.url().should('include', '/filmbokning/');
+  cy.get('.movie-details-trailer')
+    .should("be.visible")
+});
+
+Then('I should see the following information {string} {string} {string}', (movieTitle, ageLimit, language) => {
+ 
+  cy.get(' div.col-lg-6.col-xl-7.col-xxl-8 >div').should('contain', movieTitle)
+  cy.get('div.movie-details-box').should('contain', ageLimit);
+  cy.get('div.movie-details-box').should('contain', language);
+
+});
+
+When('I click on the {string} button', (backButton) => {
+cy.go(backButton)
+});
+
+Then('I should be redirected back to the home page', () => {
+  cy.url().should('eq', baseUrl)
+});
