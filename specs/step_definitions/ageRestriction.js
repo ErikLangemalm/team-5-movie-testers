bcbbca5ffe0eef55f92ea0bddf5d273a0189979e
Erik Langemalm <82160523+ErikLangemalm@users.noreply.github.com>
2024-02-07 01:01:55 +0100

    Update ageRestriction.js

--- specs/step_definitions/ageRestriction.js
+++ specs/step_definitions/ageRestriction.js
@@ -11,7 +11,7 @@ Given('I have logged in', () => {
   let email = "cifap38802@evvgo.com";
   let password = "123456";
   cy.get('#basic-nav-dropdown').click({ force: true });
-  cy.get('line-object.text-decoration-none.text-primary.dropdown-item').click({ force: true });
+  cy.get('.basic-nav-dropdown').first().click({ force: true });
   cy.get('.form-control.with-icon').eq(0).click({ force: true });
   cy.get('.form-control.with-icon').eq(0).type(email);
   cy.get('.form-control.with-icon').eq(1).click({ force: true });
