64c6bb3e952a3c83e2f50f58880503eb68fb8621
Destiny <destiny123456john@gmail.com>
2024-02-10 17:11:08 +0100

    test
    test

--- specs/step_definitions/avbokning.js
+++ specs/step_definitions/avbokning.js
@@ -1,40 +1,34 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('that I am on the first page', () => {
-  // TODO: implement step
+Given('that the website is up and running', () => {
   cy.visit('/');
   cy.viewport(1000, 1200);
 });
 
-When('I click on a timeslot', () => {
-  // TODO: implement step
-  cy.get('div.screeningContainer button').first().click({ force: true });
-});
-
-When('I click on a specifik timeslot', () => {
-  // TODO: implement step
+Given('that the user has already pre booked a seat', () => {
   cy.get('div.custom-radio-button').first().click({ force: true });
   cy.get('input.screening-input').invoke('css', 'display', 'block').should('be.visible');
   cy.get('.confirm-button').first().click()
-});
-
-When('I select vuxen', () => {
-  // TODO: implement step
-  cy.get('div.counter-container > button').eq(1).click();
-  cy.get('div > .confirm-button').first().click({ force: true });
-
-});
-
-When('I reserve a seat', () => {
-  // TODO: implement step
   cy.get('div.counter-container > button').eq(1).click();
   cy.get('div > .confirm-button').first().click({ force: true });
 });
 
-When('I put in my email', () => {
+Given('has earlier received a confirmation email', () => {
   // TODO: implement step
 });
 
-Then('That seat should be reserved for me under the bookings tab.', () => {
+When('User logs into the website using gmail and password', () => {
+  let email = "klopp@gmail.com";
+  let password = "yourPassword";
+  cy.get('.form-control.with-icon').click().type(email);
+  cy.get('.form-control.with-icon').click().type(password);
+});
+
+When('user proceeds to go into bookings', () => {
+  cy.get('div.mb-4 table-dark > button').eq(1).click();
+  cy.get('div > .cancel-booking-btn').first().click({ force: true });
+});
+
+Then('user should see previosly booked dates and should be given the option too onbook a specific movie', () => {
   // TODO: implement step
 });
